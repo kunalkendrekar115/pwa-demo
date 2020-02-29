b92fb848353301879205ef049d93bee7ccb5d4c7
Kunal Kendrekar <M1054824@mindtree.com>
2020-02-29 20:18:17 +0530

    Updates

---
+++ precache-manifest.750c8b6f144697c12904195e2f9b8495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aeeda8b84ade9f3f4731ae83d90a41d",
+    "url": "/pwa-demo/index.html"
+  },
+  {
+    "revision": "013e520af2fbbba2e73e",
+    "url": "/pwa-demo/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "571e4102fc3e8054e1d9",
+    "url": "/pwa-demo/static/js/2.4df05ac2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/pwa-demo/static/js/2.4df05ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013e520af2fbbba2e73e",
+    "url": "/pwa-demo/static/js/main.8110cffc.chunk.js"
+  },
+  {
+    "revision": "6c007d706ce717061afc",
+    "url": "/pwa-demo/static/js/runtime-main.34da9ffb.js"
+  }
+]);
