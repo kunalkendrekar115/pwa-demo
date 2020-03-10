97c45a90ae3edd50ba444fd2994947bc7efb8d1a
Kunal Kendrekar <toml.mob2016@gmail.com>
2020-03-10 15:23:28 +0530

    fcm alert

--- public/src-sw.js
+++ public/src-sw.js
@@ -7,13 +7,29 @@ importScripts("/fcmconfig.js")
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-messaging.usePublicVapidKey("BCr5QZa0YzhST1PGhAGBe-rV1ZIS_orfSUF2oo_1QASxqC4cOpgr-vCVnjHVuNb7VRvTF0_7ud5-UtcueZAMcy0");
+messaging.usePublicVapidKey(FCM_PUBLIC_KEY);
 
 console.log('this is my custom service worker');
 
-// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// messaging.setBackgroundMessageHandler(function (payload) {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     // Customize notification here
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body: 'Background Message body.',
+//         icon: '/firebase-logo.png'
+//     };
 
-// workbox.routing.registerRoute(
-//     'https://newsapi.org/v2/top-headlines?country=in&apiKey=dfcd91fa823d419c81a1cdbbf7f0f68a',
-//     new workbox.strategies.NetworkFirst({ cacheName: 'top-news' })
-// );
+//     return self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
+
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+const { routing } = workbox
+const { StaleWhileRevalidate, NetworkFirst, CacheFirst, NetworkOnly } = workbox.strategies
+
+routing.registerRoute(
+    'https://newsapi.org/v2/top-headlines?country=in&apiKey=dfcd91fa823d419c81a1cdbbf7f0f68a',
+    new StaleWhileRevalidate({ cacheName: 'top-news' })
+);
+
